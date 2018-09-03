b623477eccf79b9a8316d466b82a0ba5d0d89735
Rachel Wyatt <rw126@evansville.edu>
2018-09-03 14:47:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(collection)
-  i = 0
-  while i < collection.length
-    yield(upcase)
-    i += 1
+  return_array = []
+  collection.each do |x|
+    yield
+    return x
   end
-  return collection
+  return_array
 end
 
 def upcase(language)
